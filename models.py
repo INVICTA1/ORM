0648cfc235c6fcc3b45ededaffcd82098dde73ff
Maksim <makc.pinsk@gmail.com>
2020-12-23 18:38:24 +0300

    temp

--- models.py
+++ models.py
@@ -2,26 +2,29 @@ import peewee
 import datetime
 import databases
 
-db = peewee.SqliteDatabase('people.db')
+db = peewee.MySQLDatabase('orm', user='admin', password='admin',
+                          host='127.0.0.1', port=3306)
+
 
 class Student(peewee.Model):
     name = peewee.CharField()
     last_name = peewee.CharField()
-    year_birth = peewee.DateTimeField()
+    year_birth = peewee.DateField()
     address_residence = peewee.CharField()
-    year_admission = peewee.DateTimeField()
+    year_admission = peewee.DateField()
     group = peewee.IntegerField()
     average_score = peewee.IntegerField()
 
     class Meta:
-        database  = db
+        database = db
 
-
+    def __str__(self):
+        return str(self.id) + ' ' + self.name + ' ' + self.last_name + ' ' + str(self.year_birth) + ' ' + self.address_residence + ' ' + str(self.year_admission) + ' ' + str(self.group) + ' ' + str(self.average_score)
 
 
 if __name__ == "__main__":
     try:
         Student.create_table()
-    except peewee.OperationalError:
+    except peewee.OperationalError as e:
+        print(e)
         print("Table Student already exists!")
-
--- script.py
+++ script.py
@@ -1,18 +1,100 @@
 from datetime import date
-from models import Student
+from models import Student, db
+import logging
+import logging.config
+from peewee import Model
+# logging.basicConfig(level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
+logging.config.fileConfig(fname='app.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 
-def create_student(name, last_name, year_birth, address_residence, year_admission, group, average_score):
-    student = Student(name=name, last_name=last_name, year_birth=year_birth, address_residence=address_residence,
-                      year_admission=year_admission, group=group, average_score=average_score)
-    student.save()
+def create_student(student):
+    try:
+        student.name = str(input('Enter first name: '))
+        student.last_name = input('Enter last name: ')
+        student.year_birth = input('Enter birth date(yyyy.mm.dd): ')
+        student.address_residence = input('Enter address residence: ')
+        student.year_admission = input('Enter year admission(yyyy.mm.dd): ')
+        student.group = input('Enter group: ')
+        student.average_score = input('Enter average score: ')
+        student.save()
+        logger.info('Successful with creating student with id %s', student.id)
+    except BaseException as e:
+        logger.error('Invalid creating student', e)
 
 
+def get_student_by_id(student):
+    try:
+        id = input('Enter id')
+        student_by_id = student.get_by_id(id)
+        logger.info('Successful get student with id %s', id)
+        return student_by_id
+    except BaseException as e:
+        logger.error('Invalid entering id %s', e)
 
-def get_data_from_student():
-    student = Student.get(Student.name == 'Mask')
-    print(student)
+
+def read_all_students(student):
+    try:
+        students = []
+        for student in student.select():
+            students.append(student)
+        logger.info('Successful get all students')
+        return students
+    except BaseException as e:
+        logger.error('Invalid select * students %s', e)
+
+
+def update_student_by_id(student:Model):
+    try:
+        id = int(input('Enter id: '))
+        student = student.get_by_id(id)
+        student.name = str(input('Enter first name: '))
+        student.last_name = input('Enter last name: ')
+        student.year_birth = input('Enter birth date(yyyy.mm.dd): ')
+        student.address_residence = input('Enter address residence: ')
+        student.year_admission = input('Enter year admission(yyyy.mm.dd): ')
+        student.group = input('Enter group: ')
+        student.average_score = input('Enter average score: ')
+        student.save()
+        logger.info('Successful update student by id %s ', id)
+        return student
+    except BaseException as e:
+        logger.error('Invalid entering id %s', e)
+
+
+def delete_student_by_id(student):
+    try:
+        id = int(input('Enter id: '))
+        student.delete_by_id(id)
+        logger.info('Successful deleting student with id %s', id)
+    except BaseException as e:
+        logger.error('Invalid entering id %s', e)
+
+
+def main():
+    student = Student()
+    switcher = {
+        1: create_student,
+        2: get_student_by_id,
+        3: read_all_students,
+        4: update_student_by_id,
+        5: delete_student_by_id,
+
+    }
+    while True:
+        cmd = int(input('1 - create student\n'
+                        '2 - read students\n'
+                        '3 - read all students\n'
+                        '4 - update student\n'
+                        '5 - delete student\n'
+                        'Enter command:'))
+        response = switcher[cmd](student)
+        if type(response) == list or type(response) == tuple:
+            for row in response:
+                print(row)
+        else:
+            print(response)
+
 
 if __name__ == "__main__":
-    create_student('Mask', 'Ilon', date(1998, 12, 20), 'silikonavata dolina', 2014, 78964, 5)
-    get_data_from_student()
+    main()
